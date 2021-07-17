dd17c7288cb0629a71eb852b1cd3974edb7f6e5a
ernestochero <ernestochr08@gmail.com>
2021-07-16 21:05:42 -0500

    Mango Restaurant first implementation

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+                config.CreateMap<ProductDto, Product>().ReverseMap()
+            );
+            return mappingConfig;
+        }
+    }
+}
